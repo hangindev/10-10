8b18d41c54736bd8fb8709473ab155f9b7770b11
nrator <nrator1999@gmail.com>
2017-01-01 19:23:36 +0800

    Day7 - Parallax

---
+++ day7_Parallax/README.md
@@ -0,0 +1,10 @@
+### Day 7 of 30/30
+# Parallax
+
+[![screenshot.jpg](https://s29.postimg.org/rzmv7uzbr/screenshot.jpg)](https://postimg.org/image/t1x1qei4z/)
+
+#### Introduction
+Used a simple script to create parallax scrolling effect.
+
+#### References
+[Parallax Backgrounds](http://code.tutsplus.com/tutorials/a-simple-parallax-scrolling-technique--net-27641)
---
+++ day7_Parallax/main.js
@@ -0,0 +1,24 @@
+$(function() {
+
+	// Cache the Window object
+	var $window = $(window);
+
+	$('section[data-type="background"]').each(function(){
+		var $bgobj = $(this); // assigning the object
+
+		$(window).scroll(function() {
+
+			// Scroll the background at var speed
+			// the yPos is a negative value because we're scrolling it UP!
+			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+
+			// Put together our final background position
+			var coords = '50% '+ yPos + 'px';
+
+			// Move the background
+			$bgobj.css({ backgroundPosition: coords });
+
+		}); // end window scroll
+	});
+
+});
